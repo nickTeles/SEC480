aa751c4e39bd0579da9d65a36bafccdeef3e10d6
nickTeles <nicholas.teles@mymail.champlain.edu>
2021-10-07 11:14:21 -0400

    variables added

--- newVM.ps1
+++ newVM.ps1
@@ -1,1 +1,13 @@
-Get-VM
+#Created by Nicholas Teles
+#Variables for a new vm
+#DO NOT RUN AS SITS RIGHT NOW!!!
+$vserver="vcenter.nicholas.local.nicholas.local"
+$vm=Get-VM -Name xubuntu-wan
+$snapshot = Get-Snapshot -VM $vm -Name "Base"
+$vmhost = Get-VMHost -Name "192.168.3.126"
+$ds = Get-DataStore -Name datastore2-super7
+$linkedname = "{0}.linked" -f $vm.name
+$linkedvm = New-VM -LinkedClone -Name $linkedname -VM $vm -ReferenceSnapshot $snapshot -VMHost $vmhost -Datastore $ds
+$newvm = New-VM -Name "xubuntu-20.04-base" -VM $linkedvm -VMHost $vmhost -Datastore $ds
+$newvm | new-snapshot -Name "Base"
+$linkedvm | Remove-VM
